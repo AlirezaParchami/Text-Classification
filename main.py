cd189d0f6e25e2ac6b11c175a4077d911a938dc9
alireza parchami <alirezaprm76@gmail.com>
2019-06-18 17:12:06 +0430

    create n-gram feature

--- main.py
+++ main.py
@@ -1,33 +1,60 @@
+import nltk.classify.util
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.corpus import stopwords
+from nltk.corpus import movie_reviews
 from nltk.stem import PorterStemmer
+import csv
+from nltk.util import ngrams
+from nltk.classify import NaiveBayesClassifier
+
 
 doc_set = list()
-def read_common_words():
-    f = open("./Document/SMSSpamCollection", "r")
-    for x in f:
-        if(x[0:4] == "spam"):
-            print((x[5:]))
-        elif(x[0:3] == "ham"):
-            print((x[4:]))
-        doc_set.append(x.lower().rstrip('\n'))
-    print(doc_set)
-    print(len(doc_set))
-    f.close()
+def read_file():
+    with open('./Document/SMSSpamCollection', 'r') as csvFile:
+        reader = csv.reader(csvFile, dialect='excel', delimiter='\t')
+        for row in reader:
+            doc_set.append(row)
+        #print(doc_set)
+        #print(len(doc_set))
+    csvFile.close()
 
 def preprocessing():
-    data = "ham spam All work and no playing makes jack dull boy. All work and no play makes jack a dull boy."
-    stopWords = set(stopwords.words('english'))
-    words = word_tokenize(data)
-    print(words)
-    wordsFiltered = []
-    for w in words:
-        if w not in stopWords:
-            wordsFiltered.append(w)
-    print(wordsFiltered)
-    ps = PorterStemmer()
-    for word in wordsFiltered:
-        stem_word = ps.stem(word)
-        print(stem_word)
+    for data in doc_set:
+        stopWords = set(stopwords.words('english'))
+        words = word_tokenize(data[1])
+        #print("word tokenized: " , words)
+        wordsFiltered = []
+        for w in words:
+            if w not in stopWords:
+                wordsFiltered.append(w)
+        #print("delete stop words: ",wordsFiltered)
+        ps = PorterStemmer()
+        for i in range(len(wordsFiltered)):
+            wordsFiltered[i] = ps.stem(wordsFiltered[i])
+            #print("stem word: ",word)
+        data[1] = wordsFiltered
 
-read_common_words()
+def create_ngram_features(words, n=2):
+    ngram_vocab = ngrams(words, n)
+    my_dict = dict([(ng, True) for ng in ngram_vocab])
+    return my_dict
+
+#for fileid in movie_reviews.fileids('pos'):
+#    print(movie_reviews.words(fileid))
+read_file()
+preprocessing()
+print(doc_set)
+
+
+for n in [2]:
+    pos_data = []
+    for line in doc_set:
+        if (line[0] == "ham"):
+            words = line[1]
+            pos_data.append((create_ngram_features(words, n), "positive"))
+
+    neg_data = []
+    for line in doc_set:
+        if (line[0] == "spam"):
+            words = line[1]
+            neg_data.append((create_ngram_features(words, n), "negative"))
